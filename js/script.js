f82c3a2f8c1543750712c766e955565d93ca806b
suyud67 <suyudsetiawan@gmail.com>
2022-03-28 10:04:00 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', function () {
+  const getListItem = document.querySelector('.list-item');
+  getListItem.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
